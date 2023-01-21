bc6e6df73c8ca3d257cf58133d6e500c03055004
figarofuga <61215157+figarofuga@users.noreply.github.com>
2023-01-21 14:37:52 +0000

    make change

--- touchoku.py
+++ touchoku.py
@@ -1,4 +1,5 @@
 #%% 
+import numpy as np
 import pandas as pd
 import polars as pl
 #%%
@@ -6,6 +7,7 @@ df = pd.read_excel('to_niimi.xlsx')
 dfl = pl.DataFrame(df)
 # %%
 dfl.head()
-# %%
 
+# %%
+df_an = dfl.select([pl.col(pl.Float64).map(np.floor)]).head()
 # %%
