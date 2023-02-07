9e7f3e4cd1f4765fc7288b36b3c77f2f7b39e235
figarofuga <61215157+figarofuga@users.noreply.github.com>
2023-02-07 12:50:25 +0000

    preparation dataframes

--- touchoku.py
+++ touchoku.py
@@ -9,5 +9,11 @@ dfl = pl.DataFrame(df)
 dfl.head()
 
 # %%
-df_an = dfl.select([pl.col(pl.Float64).map(np.floor)]).head()
+df_an = (dfl.rename({'2022/04/01 (金)\n平日夜間': 'date', 
+                    '平日夜間': 'type_date'}).
+         select([pl.col("date").str.extract("(.*)(\\()", 1).str.strptime(pl.Date, fmt='%Y/%m/%d '), 
+                 pl.col("type_date"),
+                 pl.col(pl.Float64).map(np.floor)]))
+
+
 # %%
