5ae9995b58acfbbf5a9d0f1d2244b6eb4430aba2
figarofuga <figarofuga@gmail.com>
2023-02-16 13:23:44 +0000

    make better and person

--- touchoku.py
+++ touchoku.py
@@ -6,27 +6,36 @@ import polars as pl
 # %%
 df = pd.read_excel("to_niimi.xlsx")
 dfl = pl.DataFrame(df)
+
+# %%
+
+
 # %%
 df_all_null = dfl[:, [(s.null_count() == dfl.height) for s in dfl]]
 
-df_prep = (dfl[:, [not (s.null_count() == dfl.height) for s in dfl]]
-         .rename({'2022/04/01 (金)\n平日夜間': 'date',
-                    '平日夜間': 'type_date'})
-         .select([pl.col("date").str.extract("(.*)(\\()", 1).str.strptime(pl.Date, fmt='%Y/%m/%d '),
-                 pl.col("type_date"),
-                 pl.col(pl.Float64).map(np.floor)])
-         .with_column(
+df_any_null = dfl[:, [s.null_count() > 0 for s in dfl]]
+
+df_nonnull = dfl[:, [s.null_count() == 0 for s in dfl]]
+
+
+df_an = (dfl.rename({'2022/04/01 (金)\n平日夜間': 'date',
+                '平日夜間': 'type_date'})
+       .select([pl.col(pl.Float64).fill_null(-2).map(np.floor).keep_name(), 
+               pl.col("date"), 
+               pl.col("type_date")])
+       .select([pl.col("date").str.extract("(.*)(\\()", 1).str.strptime(pl.Date, fmt='%Y/%m/%d '),
+                pl.col("type_date"),
+                pl.col(pl.Float64),
+                ])
+        .with_column(
                  pl.when(pl.col("type_date") == "平日夜間").then("weekday").when(pl.col("type_date") == "休日午前").then("weekend_am").otherwise("weekend_night").alias("type_date"))
-         )
+        .with_row_count(name="row_num"))
+         
 
-
-df_any_null = df_prep[:, [s.null_count() > 0 for s in df_prep]].with_row_count(name="row_num")
-
-df_nonnull = df_prep[:, [s.null_count() == 0 for s in df_prep]].with_row_count(name="row_num")
-
-df_an = df_any_null.join(df_nonnull, on="row_num", how="left")
 # %% 
 weights = {"weekday": 1, "weekend_am": 1, "weekend_night": 2}
+
+person = df_an.select(pl.col(pl.Float64)).columns
 
 # %% [Markdown]
 # The range of numbers are -1 to 2
